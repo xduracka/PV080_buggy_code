b27c1ef294ef87f760dd114dbb3d42432712cc09
unknown <xduracka@ad.fi.muni.cz>
2025-04-24 11:05:55 +0200

    last trailing

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -12,7 +12,7 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
 class Person(object):
     def __init__(self, name):
